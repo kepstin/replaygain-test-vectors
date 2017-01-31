16a4a318d212f57128de8913a6725ed697a5ed53
Calvin Walton <calvin.walton@kepstin.ca>
2017-01-30 23:31:01 -0500

    Support for Ogg Opus style R128 gain tags

--- tagger.py
+++ tagger.py
@@ -37,24 +37,24 @@ TG_UPPER='REPLAYGAIN_TRACK_GAIN'
 TP_UPPER='REPLAYGAIN_TRACK_PEAK'
 AG_UPPER='REPLAYGAIN_ALBUM_GAIN'
 AP_UPPER='REPLAYGAIN_ALBUM_PEAK'
-
-# lowercase as used in ... well, nothing really.
-TG_LOWER='replaygain_track_gain'
-TP_LOWER='replaygain_track_peak'
-AG_LOWER='replaygain_album_gain'
-AP_LOWER='replaygain_album_peak'
+R128_TG_UPPER='R128_TRACK_GAIN'
+R128_AG_UPPER='R128_ALBUM_GAIN'
 
 # 100% guaranteed random case variations
 TG_MIXED='rEpLaYGaIn_TrAcK_gAiN'
 TP_MIXED='RePLaYgAIn_tRAcK_pEAk'
 AG_MIXED='RePlAYgAIn_aLbUM_GAin'
 AP_MIXED='rEPlayGaiN_AlBum_PeaK'
+R128_TG_MIXED='r128_trAcK_gAiN'
+R128_AG_MIXED='R128_aLbUM_GAin'
 
 # The default tag formats (can be overridden by options)
 TG=TG_UPPER
 TP=TP_UPPER
 AG=AG_UPPER
 AP=AP_UPPER
+R128_TG=R128_TG_UPPER
+R128_AG=R128_AG_UPPER
 
 # Format ReplayGain gain adjustment values, either in the standard way
 # or with some non-standard distortions
@@ -69,6 +69,20 @@ def format_rg_peak(peak, args):
     # "Six numeric digits in the decimal field (dddddd) is adequate to
     # accurately represent peak values for 16-bit audio data."
     return "{:.6f}".format(10.0 ** (peak / 20.0))
+
+# Format Ogg Opus R128 gain adjustment values
+def format_oggopus_gain(gain, args):
+    # The R128 gain tags use a -23LUFS reference rather than -18LUFS
+    gain = gain + 5
+    # And is a fixed-point value represented as an integer
+    gain = int(gain * 256.0)
+
+    if gain < -32768 or gain > 32767:
+        logger.warning("Opus gain value %d is out of range", gain)
+
+    # Standard format
+    return "{:d}".format(value)
+
 
 def write_id3(id3, args):
     if args.id3_txxx:
@@ -115,6 +129,18 @@ def write_generic(tags, args):
     if args.ap is not None:
         tags[AP] = [format_rg_peak(args.ap, args)]
 
+def write_vorbiscomment(tags, args):
+    if args.vc_standard:
+        write_generic(tags, args)
+
+    if args.vc_oggopus:
+        if args.tp is not None or args.ap is not None:
+            logger.warning("Ogg Opus R128 gain tags do not support peak info")
+        
+        if args.tg is not None:
+            tags[R128_TG] = [format_oggopus_gain(args.tg, args)]
+        if args.ag is not None:
+            tags[R128_AG] = [format_oggopus_gain(args.ag, args)]
 
 def write_mp3(args):
     mp3 = mutagen.mp3.MP3(args.file)
@@ -128,7 +154,7 @@ def write_mp3(args):
 
 def write_oggvorbis(args):
     oggvorbis = mutagen.oggvorbis.OggVorbis(args.file)
-    write_generic(oggvorbis.tags, args)
+    write_vorbiscomment(oggvorbis.tags, args)
     oggvorbis.save()
 
 parser = argparse.ArgumentParser(description='ReplayGain tag testing tool')
@@ -172,6 +198,12 @@ parser.add_argument('--mp3-info', action='store_true',
 parser.add_argument('--mp3-apev2', action='store_true',
         help='write gain values to an APEv2 tag on MP3 (like mp3gain)')
 
+# Multiple formats - VorbisComment
+parser.add_argument('--vc', dest='vc_standard', action='store_true',
+        help='write VorbisComment tags in standard ReplayGain format')
+parser.add_argument('--vc-oggopus', action='store_true',
+        help='write VorbisComment tags in Ogg Opus R128 format')
+
 args = parser.parse_args()
 
 if args.mixed_case:
@@ -179,6 +211,8 @@ if args.mixed_case:
     TP=TP_MIXED
     AG=AG_MIXED
     AP=AP_MIXED
+    R128_TG=R128_TG_MIXED
+    R128_AG=R128_AG_MIXED
 
 for format in args.formats:
     format(args)
